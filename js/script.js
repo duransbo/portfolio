b248d25c16e6960da28f024733320020087f2609
Gustavo Duran Ferreira <guduran159@gmail.com>
2016-08-28 12:10:28 -0300

    alteração 2

--- js/script.js
+++ js/script.js
@@ -1,102 +1,128 @@
-document.getElementById('audio').volume = 1;
+document.querySelector('.home-music').volume = 1;
 context = new AudioContext();
 analyser = context.createAnalyser();
-source = context.createMediaElementSource(document.getElementById('audio'));
+source = context.createMediaElementSource(document.querySelector('.home-music'));
 source.connect(analyser);
 analyser.connect(context.destination);
 frec_array = new Uint8Array(analyser.frequencyBinCount);
 
-canvasId = document.getElementById('home-bg');
-canvas = canvasId.getContext('2d');
-barInterval = 32;
-canvasD();
+homeBg = document.querySelector('.home-bg');
+document.querySelector('.home-music-play').addEventListener("click", musicStart);
 
-window.onresize = function(){
+window.onload = function() {
 	canvasD();
+	funcAnimation();
+	parallaxLogo();
+	document.querySelector('.home-logo').style.opacity = 1;
 }
 
-window.onscroll = function(){
+window.onresize = function() {
+	canvasD();
 	parallaxLogo();
 }
 
-function canvasD(){
-	canvasId.width = document.getElementById('home').clientWidth - 1;
-	canvasId.height = document.getElementById('home').clientHeight;
-	widthEnd = canvasId.width;
-	heightEnd = canvasId.height;
+window.onscroll = function() {
+	parallaxLogo();
+}
+
+
+function musicStart() {
+	if (document.querySelector('.home-music').paused) {
+	 	document.querySelector('.home-music').play();
+	 	document.querySelector('.home-music-play').innerHTML =  "&#xf04c";
+    } else {
+    	document.querySelector('.home-music').pause();
+	 	document.querySelector('.home-music-play').innerHTML =  "&#xf04b";
+    }
+}
+
+
+function canvasD() {
+	homeBg.width = document.querySelector('.home').clientWidth - 1;
+	homeBg.height = document.querySelector('.home').clientHeight;
+	widthEnd = homeBg.width;
+	heightEnd = homeBg.height;
 	widthCenter = widthEnd / 2;
 	barSpace = widthEnd / (analyser.frequencyBinCount * 2);
 }
 
-function parallaxLogo(){
+function parallaxLogo() {
 	parallax(
-		0,
-		document.getElementById('home').clientHeight,
+		(document.querySelector('.home').clientHeight / 2) - (document.querySelector('.home-logo').clientHeight / 2),
+		document.querySelector('.home').clientHeight,
 		"tl",
-		document.querySelector('.circle'),
-		(document.getElementById('home').clientWidth / 2) - (document.querySelector('.circle').clientWidth / 2),
-		(document.getElementById('home').clientHeight / 2) - (document.querySelector('.circle').clientHeight / 2),
-		(document.getElementById('home').clientWidth / 2) - (document.querySelector('.circle').clientWidth / 2),
-		(window.innerHeight / 2) - (document.querySelector('.circle').clientHeight / 2),
+		document.querySelector('.home-logo'),
+		(document.querySelector('.home').clientWidth / 2) - (document.querySelector('.home-logo').clientWidth / 2),
+		(document.querySelector('.home').clientHeight / 2) - (document.querySelector('.home-logo').clientHeight / 2),
+		(document.querySelector('.home').clientWidth / 2) - (document.querySelector('.home-logo').clientWidth / 2),
+		(window.innerHeight / 2) - (document.querySelector('.home-logo').clientHeight / 2),
 		"px"
 	);
 }
 
-funcAnimation();
-
-function funcAnimation(){
+function funcAnimation() {
 	requestAnimationFrame(funcAnimation);
 
 	analyser.getByteFrequencyData(frec_array);
-	canvas.fillStyle = 'rgb(' + parseInt(frec_array[0] / 8) + ',' + parseInt((frec_array[0] / 8) * (32 / 21)) + ',' + parseInt((frec_array[0] / 8) * (54 / 21)) + ')';
-	canvas.fillRect(0, 0, widthEnd, heightEnd);
-	canvas.fillStyle = '#f1e5df';
-	canvas.beginPath();
-	canvas.moveTo(widthCenter, heightEnd - frec_array[0]);
-	for(i = 1; i < analyser.frequencyBinCount; i += barInterval){
-		canvas.quadraticCurveTo(
+	barInterval = 8;
+	f = 0;
+	d = 8;
+	homeBg.getContext('2d').fillStyle = 'rgb(' + parseInt(frec_array[f] / d) + ',' + parseInt((frec_array[f] / d) * (32 / 21)) + ',' + parseInt((frec_array[f] / d) * (54 / 21)) + ')';
+	homeBg.getContext('2d').fillRect(0, 0, widthEnd, heightEnd);
+	homeBg.getContext('2d').fillStyle = '#f1e5df';
+	homeBg.getContext('2d').beginPath();
+	homeBg.getContext('2d').moveTo(widthCenter, heightEnd - frec_array[0]);
+	for (i = 1; i < analyser.frequencyBinCount; i += barInterval) {
+		homeBg.getContext('2d').quadraticCurveTo(
 			widthCenter + ((i - (barInterval / 2)) * barSpace),
 			heightEnd - frec_array[i] - ((frec_array[i - barInterval] - frec_array[i]) / 2),
 			widthCenter + (i * barSpace),
 			heightEnd - frec_array[i]
 		);
 	}
-	canvas.lineTo(widthCenter + i, heightEnd);
-	canvas.lineTo(widthCenter, heightEnd);
-	canvas.moveTo(widthCenter, heightEnd - frec_array[0]);
-	for(i = 1; i < analyser.frequencyBinCount; i += barInterval){
-		canvas.quadraticCurveTo(
+	homeBg.getContext('2d').lineTo(widthCenter + i, heightEnd);
+	homeBg.getContext('2d').lineTo(widthCenter, heightEnd);
+	homeBg.getContext('2d').moveTo(widthCenter, heightEnd - frec_array[0]);
+	for (i = 1; i < analyser.frequencyBinCount; i += barInterval) {
+		homeBg.getContext('2d').quadraticCurveTo(
 			widthCenter - ((i - (barInterval / 2)) * barSpace),
 			heightEnd - frec_array[i] - ((frec_array[i - barInterval] - frec_array[i]) / 2),
 			widthCenter - (i * barSpace),
 			heightEnd - frec_array[i]
 		);
 	}
-	canvas.lineTo(widthCenter - i, heightEnd);
-	canvas.lineTo(widthCenter, heightEnd);
-	canvas.fill();
+	homeBg.getContext('2d').lineTo(widthCenter - i, heightEnd);
+	homeBg.getContext('2d').lineTo(widthCenter, heightEnd);
+	homeBg.getContext('2d').fill();
 }
 
-function parallax(iniP,endP,type,div,iniX,iniY,endX,endY,un,unY=un){
+function parallax(iniP,endP,type,div,iniX,iniY,endX,endY,un,unY=un) {
+
 	scroll = window.pageYOffset || document.documentElement.scrollTop;
-	if((scroll >= iniP) && (scroll <= endP)){
+	if ((scroll >= iniP) && (scroll <= endP)) {
 		move = ((scroll - iniP) * 100) / (endP - iniP);
 		moveX = (iniX - (((iniX - endX) * move) / 100)) + un;
 		moveY = (iniY - (((iniY - endY) * move) / 100)) + unY;
+	} else if (scroll < iniP) {
+		moveX = iniX + un;
+		moveY = iniY + unY;
+	} else {
+		moveX = endX + un;
+		moveY = endY + unY;
+	}
 
-		switch(type){
-			case 'bg':
-				div.style.backgroundPosition = moveX + ' ' + moveY;
-				break;
+	switch (type) {
+		case 'bg':
+			div.style.backgroundPosition = moveX + ' ' + moveY;
+			break;
 
-			case 'tl':
-				div.style.top = moveY;
-				div.style.left = moveX;
-				break;
+		case 'tl':
+			div.style.top = moveY;
+			div.style.left = moveX;
+			break;
 
-			default:
-				alert('Type parallax|' + id + '| not valid');
-				break;
-		}
+		default:
+			alert('Type parallax|' + id + '| not valid');
+			break;
 	}
 }
