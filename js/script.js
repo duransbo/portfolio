6b93e55e281033f6072ca930885d4988a4eb2c5b
Gustavo Duran Ferreira <guduran159@gmail.com>
2016-09-24 12:28:29 -0300

    alteração 9

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
-document.querySelector('.menu-control').addEventListener("click", menuControl);
-document.querySelector('.home-music-control b').addEventListener("click", musicControl);
+getElement('.menu-control').addEventListener("click", menuControl);
+getElement('.home-music-control b').addEventListener("click", musicControl);
 
 
 
 window.onload = function () {
-	loadCss('css/style.css',function () {
-		var loading = document.querySelector('.loading');
+	loadCss('css/base.css');
+	loadCss('css/style.css', function () {
+		var loading = getElement('.loading');
 
 		loading.innerHTML = "<p>Bem-vindo</p>";
+		musicSinc();
+		menuControl();
 		setTimeout(function () {
-			musicSinc();
-			menuControl();
-			parallaxLogo();
 			loading.style.opacity = 0;
 			setTimeout(function () {
 				loading.style.display = 'none';
-				document.querySelector('.home-logo').style.opacity = 1;
+				getElement('.home-logo').style.opacity = 1;
 				musicControl();
 			}, 500);
-		}, 1000);
+		}, 500);
 	});
 }
 
 
 
-window.onresize = function() {
+window.onresize = function () {
 	canvasD();
 	parallaxLogo();
 }
 
 
 
-window.onscroll = function() {
+window.onscroll = function () {
 	parallaxLogo();
 
-	var slogan = document.querySelector('.slogan');
-	var logo = document.querySelector('.home-logo');
+	var slogan = getElement('.slogan');
+	var logo = getElement('.home-logo');
 	if (
 		slogan.classList.contains('ini') && (
 			scroll > (
-				document.querySelector('.home').clientHeight +
+				getElement('.home').clientHeight +
 				(slogan.clientHeight / 2) -
 				(logo.clientHeight / 2) -
 				parseInt(logo.style.top) +
@@ -53,28 +53,28 @@ window.onscroll = function() {
 
 
 
-function loadCss(url, callback) {
+function loadCss(url, callback = function () {}) {
 	var css = document.createElement('link');
     css.setAttribute('type', "text/css" );
     css.setAttribute('rel', "stylesheet" );
     css.setAttribute('href', url );
 	css.onload = callback;
-    document.getElementsByTagName("head").item(0).appendChild(css);
+    document.getElementsByTagName('head').item(0).appendChild(css);
 }
 
 
 
 function menuControl() {
-	var master = document.querySelector('.master').classList;
-	var menuControl = document.querySelector('.menu-control > b');
-	var logoStyle = document.querySelector('.home-logo').style;
+	var master = getElement('.master').classList;
+	var menuControl = getElement('.menu-control > b');
+	var logoStyle = getElement('.home-logo').style;
 	if (master.contains('on-menu')) {
 		master.remove('on-menu');
 		menuHeight = 0;
 		menuControl.innerHTML = '&#xf0c9;';
 	} else {
 		master.add('on-menu');
-		menuHeight = parseInt(document.querySelector('.menu-control').clientHeight);
+		menuHeight = parseInt(getElement('.menu-control').clientHeight);
 		menuControl.innerHTML = '&#xf00d;';
 	}
 	logoStyle.transition = 'top .5s';
@@ -89,11 +89,11 @@ function menuControl() {
 function musicSinc() {
 	context = new AudioContext();
 	analyser = context.createAnalyser();
-	source = context.createMediaElementSource(document.querySelector('.home-music'));
+	source = context.createMediaElementSource(getElement('.home-music'));
 	source.connect(analyser);
 	analyser.connect(context.destination);
 	frec_array = new Uint8Array(analyser.frequencyBinCount);
-	homeBg = document.querySelector('.home-bg');
+	homeBg = getElement('.home-bg');
 	canvasD();
 	funcAnimation();
 }
@@ -101,8 +101,8 @@ function musicSinc() {
 
 
 function musicControl() {
-	var music = document.querySelector('.home-music');
-	var musicControl = document.querySelector('.home-music-control b');
+	var music = getElement('.home-music');
+	var musicControl = getElement('.home-music-control b');
 
 	if (music.paused) {
 	 	music.play();
@@ -116,8 +116,8 @@ function musicControl() {
 
 
 function canvasD() {
-	homeBg.width = document.querySelector('.home').clientWidth;
-	homeBg.height = document.querySelector('.home').clientHeight;
+	homeBg.width = getElement('.home').clientWidth;
+	homeBg.height = getElement('.home').clientHeight;
 	widthEnd = homeBg.width;
 	heightEnd = homeBg.height;
 	widthCenter = widthEnd / 2;
@@ -128,8 +128,8 @@ function canvasD() {
 
 function parallaxLogo() {
 	scroll = window.pageYOffset;
-	var home = document.querySelector('.home');
-	var logo = document.querySelector('.home-logo');
+	var home = getElement('.home');
+	var logo = getElement('.home-logo');
 
 	parallax(
 		(home.clientHeight / 2) - (logo.clientHeight / 2) + menuHeight,
@@ -153,34 +153,35 @@ function funcAnimation() {
 	barInterval = 8;
 	f = 0;
 	d = 8;
-	homeBg.getContext('2d').clearRect(0,0,widthEnd,heightEnd);
-	homeBg.getContext('2d').fillStyle = 'rgba(' + parseInt(frec_array[f] / d) + ',' + parseInt((frec_array[f] / d) * (32 / 21)) + ',' + parseInt((frec_array[f] / d) * (54 / 21)) + ',' + (1 - (frec_array[f] / 500)) + ')';
-	homeBg.getContext('2d').fillRect(0, 0, widthEnd, heightEnd);
-	homeBg.getContext('2d').fillStyle = 'rgb(241, 228, 222)';
-	homeBg.getContext('2d').beginPath();
-	homeBg.getContext('2d').moveTo(widthCenter, heightEnd - frec_array[0]);
+	canvasCtx = homeBg.getContext('2d');
+	canvasCtx.clearRect(0,0,widthEnd,heightEnd);
+	canvasCtx.fillStyle = 'rgba(' + parseInt(frec_array[f] / d) + ',' + parseInt((frec_array[f] / d) * (32 / 21)) + ',' + parseInt((frec_array[f] / d) * (54 / 21)) + ',' + (1 - (frec_array[f] / 500)) + ')';
+	canvasCtx.fillRect(0, 0, widthEnd, heightEnd);
+	canvasCtx.fillStyle = 'rgb(241, 228, 222)';
+	canvasCtx.beginPath();
+	canvasCtx.moveTo(widthCenter, heightEnd - frec_array[0]);
 	for (i = 1; i < analyser.frequencyBinCount; i += barInterval) {
-		homeBg.getContext('2d').quadraticCurveTo(
+		canvasCtx.quadraticCurveTo(
 			widthCenter + ((i - (barInterval / 2)) * barSpace),
 			heightEnd - frec_array[i] - ((frec_array[i - barInterval] - frec_array[i]) / 2),
 			widthCenter + (i * barSpace),
 			heightEnd - frec_array[i]
 		);
 	}
-	homeBg.getContext('2d').lineTo(widthCenter + i, heightEnd);
-	homeBg.getContext('2d').lineTo(widthCenter, heightEnd);
-	homeBg.getContext('2d').moveTo(widthCenter, heightEnd - frec_array[0]);
+	canvasCtx.lineTo(widthCenter + i, heightEnd);
+	canvasCtx.lineTo(widthCenter, heightEnd);
+	canvasCtx.moveTo(widthCenter, heightEnd - frec_array[0]);
 	for (i = 1; i < analyser.frequencyBinCount; i += barInterval) {
-		homeBg.getContext('2d').quadraticCurveTo(
+		canvasCtx.quadraticCurveTo(
 			widthCenter - ((i - (barInterval / 2)) * barSpace),
 			heightEnd - frec_array[i] - ((frec_array[i - barInterval] - frec_array[i]) / 2),
 			widthCenter - (i * barSpace),
 			heightEnd - frec_array[i]
 		);
 	}
-	homeBg.getContext('2d').lineTo(widthCenter - i, heightEnd);
-	homeBg.getContext('2d').lineTo(widthCenter, heightEnd);
-	homeBg.getContext('2d').fill();
+	canvasCtx.lineTo(widthCenter - i, heightEnd);
+	canvasCtx.lineTo(widthCenter, heightEnd);
+	canvasCtx.fill();
 }
 
 
@@ -213,4 +214,10 @@ function parallax(iniP,endP,type,div,iniX,iniY,endX,endY,un,unY=un) {
 			alert('Type parallax|' + id + '| not valid');
 			break;
 	}
+}
+
+
+
+function getElement(id) {
+	return document.querySelector(id);
 }
